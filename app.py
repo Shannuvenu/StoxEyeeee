834ea4965b696d447c3055394011237f5a930b2a
shannuvenu <venu98865@gmail.com>
2025-07-31 08:47:58 +0530

    Finalized StoxEye with pie chart updates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from fetch_data import get_stock_data
 from alerts import check_price_alert, check_volume_alert
 from portfolio_optimizer import fetch_data, optimize_portfolio
+import matplotlib.pyplot as plt 
 st.set_page_config(page_title="StoxEye", layout="wide")
 st.title("📈 StoxEye – Real-Time Stock Dashboard")
 st.markdown("Track. Analyze. Grow your investments like a pro.")
@@ -44,6 +45,15 @@ if df is not None:
         st.write(" Optimal Weights:")
         for sym, w in zip(symbols, result["weights"]):
             st.write(f"{sym}: {w * 100:.2f}%")
+        fig,ax=plt.subplots(figsize=(2,1))
+        wedges,texts,autotexts=ax.pie(result["weights"],labels=symbols,autopct='%1.1f%%',startangle=90,textprops={'fontsize':10},labeldistance=1.2,pctdistance=0.7)
+        for text in texts:
+            text.set_fontweight('bold')
+        for autotext in autotexts:
+            autotext.set_fontsize(9)    
+        ax.axis('equal')
+        st.subheader("portfolio allocation pie chart")
+        st.pyplot(fig)
         st.write(f"Expected Return: `{result['expected_return']:.2%}`")
         st.write(f"Expected Risk: `{result['expected_risk']:.2%}`")
     else:
